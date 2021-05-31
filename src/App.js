8fab0d4c901fabe3d9a5bdc26d3bc5a50dedd411
DenysPyshniuk <denys.pyshniuk@gmail.com>
2021-05-31 09:52:58 -0700

    Wrap whole app into ApolloProvider

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+import React from "react";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "@apollo/react-hooks";
+
 function App() {
-  return <p>Hello</p>;
+  const client = new ApolloClient({
+    uri: "https://graphql-pokemon2.vercel.app/",
+  });
+
+  return (
+    <ApolloProvider client={client}>
+      <main>
+        <p>I am a Pokemon!</p>
+      </main>
+    </ApolloProvider>
+  );
 }
 
 export default App;
